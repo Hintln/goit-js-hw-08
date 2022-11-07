a68649a555a1479c1fe9e2714525ad650fe0488f
Hintln <andrejlityprog@gmail.com>
2022-11-07 14:15:33 +0200

    input+

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,5 +5,13 @@ const STORAGE_KEY = "feedback-form-state";
 const formData = {};
 const feedbackForm = document.querySelector('.feedback-form');
 
+feedbackForm.addEventListener('input', throttle(onFormImput, 500));
+
+
+function onFormImput(e) {
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
 
 
