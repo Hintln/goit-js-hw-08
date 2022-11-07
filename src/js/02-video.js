4ffb48aa30e465907e83d4e783e4e753bb56776b
Hintln <andrejlityprog@gmail.com>
2022-11-07 11:18:00 +0200

    2/

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+
+
+const playOn = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+  localStorage.setItem(playOn, e.seconds);
+}
+
+setCurrenTime();
+
+function setCurrenTime() {
+  const currentTime = localStorage.getItem(playOn);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
